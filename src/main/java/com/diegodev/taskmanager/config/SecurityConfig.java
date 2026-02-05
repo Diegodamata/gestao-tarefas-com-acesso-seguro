a80975b6b9553a37b448a8f2fcaba3e5256c35bd
Diego da mata <Diegodamata>
2026-02-05 20:40:36 -0300

    feat: custom de userDetailsService

--- src/main/java/com/diegodev/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/diegodev/taskmanager/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.diegodev.taskmanager.config;
 
+import com.diegodev.taskmanager.security.CustomUserDetailsService;
+import com.diegodev.taskmanager.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,5 +36,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserService userService){
+        return new CustomUserDetailsService(userService);
     }
 }
---
+++ src/main/java/com/diegodev/taskmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.diegodev.taskmanager.security;
+
+import com.diegodev.taskmanager.domain.User;
+import com.diegodev.taskmanager.services.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+
+        User user = userService.findByLogin(login);
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getLogin())
+                .password(user.getPassword())
+                .roles(user.getRoles().toString())
+                .build();
+    }
+}
