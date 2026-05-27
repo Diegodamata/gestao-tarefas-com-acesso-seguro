9e7f0d6eb12dde95d1a7e5fccae36b57cf4a9eb5
Diego <Diegodamata>
2026-05-26 21:32:00 -0300

    Documentando a camada roleController

--- src/main/java/com/diegodev/taskmanager/controllers/RoleController.java
+++ src/main/java/com/diegodev/taskmanager/controllers/RoleController.java
@@ -5,6 +5,10 @@ import com.diegodev.taskmanager.controllers.dtos.role.responses.RoleResponseDto;
 import com.diegodev.taskmanager.controllers.mappers.role.RoleMapper;
 import com.diegodev.taskmanager.domain.Role;
 import com.diegodev.taskmanager.services.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/roles")
+@Tag(name = "Roles")
 public class RoleController {
 
     private final RoleService roleService;
@@ -28,6 +33,12 @@ public class RoleController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Criar", description = "Criar uma role")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Cadastrando uma nova role"),
+            @ApiResponse(responseCode = "422", description = "Erro de validação"),
+            @ApiResponse(responseCode = "409", description = "Role já cadastrada")
+    })
     public ResponseEntity<RoleResponseDto> created(@RequestBody @Valid RoleRequestDto roleRequestDto){
 
         Role role = roleService.created(roleMapper.toEntity(roleRequestDto));
@@ -43,6 +54,10 @@ public class RoleController {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Buscar", description = "Listar todas as roles")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Roles listadas com sucesso")
+    })
     public ResponseEntity<List<RoleResponseDto>> reading(){
 
         List<Role> roles = roleService.reading();
@@ -52,6 +67,12 @@ public class RoleController {
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Atualizar", description = "Atualizar uma role")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Atualizado com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Erro de validação"),
+            @ApiResponse(responseCode = "404", description = "Role não encontrada")
+    })
     public ResponseEntity<RoleResponseDto> update(@RequestBody @Valid RoleRequestDto roleRequestDto,
                                                   @PathVariable("id") Long id){
         Role roleUpdate = roleService.update(roleMapper.toEntity(roleRequestDto), id);
@@ -61,6 +82,10 @@ public class RoleController {
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Deletar", description = "Deletando uma role")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Deletado com sucesso")
+    })
     public ResponseEntity<Void> delete(@PathVariable("id") Long id){
         roleService.delete(id);
         return ResponseEntity.ok().build();
