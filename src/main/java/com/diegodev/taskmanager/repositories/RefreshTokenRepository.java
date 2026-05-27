b3eaba0983829c4a6835047d90afd1ae3866f84f
Diego <Diegodamata>
2026-05-26 21:49:06 -0300

    Criando dois metodos para delecao do refresh token

--- src/main/java/com/diegodev/taskmanager/repositories/RefreshTokenRepository.java
+++ src/main/java/com/diegodev/taskmanager/repositories/RefreshTokenRepository.java
@@ -3,9 +3,12 @@ package com.diegodev.taskmanager.repositories;
 import com.diegodev.taskmanager.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String refreshToken);
+    void deleteByRevokedTrue();
+    void deleteByExpiresInBefore(Instant now);
 }
