7d6a01f7cc38030dc6d62a63a2c6ff1af7c98f34
Diego <Diegodamata>
2026-06-01 21:01:22 -0300

    Habilitando cores de log no console

--- src/main/java/com/diegodev/taskmanager/controllers/UserController.java
+++ src/main/java/com/diegodev/taskmanager/controllers/UserController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -28,12 +29,13 @@ public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
-    private final Logger logger;
 
-    public UserController(UserService userService, UserMapper userMapper, Logger logger) {
+    private static final Logger logger
+            = LoggerFactory.getLogger(UserController.class);
+
+    public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
         this.userMapper = userMapper;
-        this.logger = logger;
     }
 
     @PostMapping
